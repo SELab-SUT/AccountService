5f79725851a855f78bf3bf1cff65769ca54e4cd7
Alireza Mousavi <moosavi78@gmail.com>
2021-04-23 16:33:39 +0430

    Add sqlalchemy and all_users query

--- account_server.py
+++ account_server.py
@@ -2,60 +2,38 @@ import sqlite3
 import os
 from flask import Flask, request, g
 from flask.json import jsonify
+from sqlalchemy.exc import IntegrityError
 from http import HTTPStatus
 from werkzeug.security import generate_password_hash
+from models import *
 
 app = Flask(__name__)
-DATABASE = 'db.sqlite3'
-
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
-def get_db():
-	db = getattr(g, '_database', None)
-	if db is None:
-		db = g._database = sqlite3.connect(DATABASE)
-		db.row_factory = dict_factory
-	return db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False	# To silent a warning
 
 
 @app.before_first_request
 def setup_db():
-	cur = get_db().cursor()
-
-	cur.execute('''CREATE TABLE IF NOT EXISTS users 
-									(username VARCHAR(15) NOT NULL PRIMARY KEY,
-									hashed_passwd TEXT NOT NULL,
-									email VARCHAR(320),
-									phone VARCHAR(15),
-									isAdmin BOOLEAN NOT NULL)''')
-	passwd_hashed = generate_password_hash('1234', method='sha256')
-	cur.execute("""INSERT OR IGNORE INTO users VALUES 
-						('superadmin', ?, NULL, NULL, TRUE)""", 
-						(passwd_hashed,))
-	get_db().commit()
+	db.init_app(app)
+	db.create_all()
 
 
 @app.route('/create_user', methods=['POST'])
 def create_user():
 	data = request.json
-	cur = get_db().cursor()
+
+	user = User(username=data.get('username'),
+				hashed_passwd=data.get('hashed_passwd'),
+				email=data.get('email'),
+				phone=data.get('phone'),
+				isAdmin=data.get('isAdmin'))
 
 	try:
-		cur.execute('INSERT INTO users VALUES (?, ?, ?, ?, ?)', (data.get('username'),
-															data.get('hashed_passwd'),
-															data.get('email'),
-															data.get('phone'),
-															data.get('isAdmin')))
-		get_db().commit()
+		db.session.add(user)
+		db.session.commit()
 		return {'message': 'Success'}, HTTPStatus.CREATED	
 	
-	except sqlite3.IntegrityError as e:
+	except IntegrityError as e:
 		if 'UNIQUE' in str(e):
 			return {'message': 'Error: username already exists'}, HTTPStatus.CONFLICT
 		else:
@@ -64,47 +42,41 @@ def create_user():
 
 @app.route('/get_user/<username>')
 def get_user(username):
-	cur = get_db().cursor()
-	cur.execute('SELECT * FROM users WHERE username = ?', (username,))
-	row = cur.fetchone()
+	user = User.query.get(username)
 
-	if row is None:
+	if user is None:
 		return {'message': 'Error: No user found'}, HTTPStatus.NOT_FOUND
-	else:	
-		return {'message': 'Success', 'user': row}, HTTPStatus.OK
+	else:
+		return {'message': 'Success', 'user': user.to_dict()}, HTTPStatus.OK
 
 
 @app.route('/modify_user/<username>', methods=['PUT'])
 def modify_user(username):
 	data = request.json
-	cur = get_db().cursor()
-	val_list = []
+	user = User.query.get(username)
+
+	if user is None:
+		return {'message': 'Error: No user found'}, HTTPStatus.NOT_FOUND
+
+	if 'hashed_passwd' in data:
+		user.hashed_passwd = data['hashed_passwd']
+	if 'email' in data:
+		user.email = data['email']
+	if 'phone' in data:
+		user.phone = data['phone']
+	if 'isAdmin' in data:
+		user.isAdmin = data['isAdmin']
+
 	try:
-		for col in data:
-			if col == 'isAdmin':
-				val_list.append(f'{col} = {data[col]}')
-			else:
-				val_list.append(f"{col} = '{data[col]}'")
-
-		command = 'UPDATE users SET ' + ','.join(val for val in val_list) + \
-					' WHERE username = ?'
-		cur.execute(command, (username,))
-		get_db().commit()
-
-		if cur.rowcount == 0:
-			return {'message': 'Error: No user found'}, HTTPStatus.NOT_FOUND
-		else:
-			return {'message': 'Success'}, HTTPStatus.OK
-
-	except sqlite3.IntegrityError:
+		db.session.commit()
+		return {'message': 'Success'}, HTTPStatus.OK
+	except IntegrityError:
 		return {'message': 'Bad fields'}, HTTPStatus.BAD_REQUEST
 
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+@app.route('/all_users', methods=['GET'])
+def all_users():
+	return jsonify([u.to_dict() for u in User.query.all()])
 
 
 if __name__=='__main__':
---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+	username = db.Column(db.String(15), primary_key=True)
+	hashed_passwd = db.Column(db.Text, nullable=False)
+	email = db.Column(db.String(320))
+	phone = db.Column(db.String(15))
+	isAdmin = db.Column(db.Boolean, nullable=False)
+
+	def to_dict(self):
+		vals = vars(self)
+		return {attr: str(vals[attr]) for attr in vals if 'instance_state' not in attr}
